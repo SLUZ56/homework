3027c4a6369d8e8abb6a8d49830e327ff29b080c
SLUZ56 <99044741+SLUZ56@users.noreply.github.com>
2022-02-28 19:41:20 -0300

    I pushed a new file
    I pushed a new file

---
+++ exerc6.js
@@ -0,0 +1,15 @@
+/* Faça um programa onde os jurados vão dar a explicação para cada nota dada.
+ São permitidas notas de 0 a 10. Cada nota deve conter uma mensagem diferente.
+*/
+const score = 0
+switch (score) {
+    case 0:
+        console.log('Foi muito Ruim!!!')
+        break;
+    case 10:
+        console.log('Foi muito BOM!!!')
+        break;
+
+    default:
+        break;
+}
