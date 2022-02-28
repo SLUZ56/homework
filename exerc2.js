d0f7031041c33ea3c37e5c0175b584daf99dee4c
SLUZ56 <99044741+SLUZ56@users.noreply.github.com>
2022-02-28 19:39:37 -0300

    I pushed a new file
    I pushed a new file

---
+++ exerc2.js
@@ -0,0 +1,14 @@
+/*
+Faça um programa que verifica se uma pessoa é brasileira ou não.
+ // DEVE SER UM OBJETO
+
+*/
+const person = {
+    name: 'João',
+    nationality: 'russian'
+}
+if(person.nationality === 'brasileiro'){
+    console.log('A pessoa é brasileira.')
+} else {
+    console.log('A pessoa é estrangeira.')
+}
