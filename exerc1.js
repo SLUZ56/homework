b878cfa6ad18d11e20c50a9bb4a96c805cbd55e6
SLUZ56 <99044741+SLUZ56@users.noreply.github.com>
2022-02-28 19:39:05 -0300

    I pushed a new file
    I pushed a new file

---
+++ exerc1.js
@@ -0,0 +1,12 @@
+/* Faça um programa que compare um número com 10 e imprime na tela "o número é maior,
+ o número é menor o número é igual"
+ */
+const number = 12
+if(number < 10){
+    console.log('O número é menor que 10.')
+} else
+   if(number > 10){
+       console.log('O número é maior que 10')
+   } else {
+       console.log('O número é igual a 10.')
+   }
